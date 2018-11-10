759161a670776c7968b4e37def73021e28164452
zhakina90 <zhakina90@gmail.com>
2018-11-10 15:07:24 -0600

    added more work

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+var compLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
+  var userInput = [];
+  var userGuesses = 9; 
+  var userLosses = 0;
+  var userWins = 0;
+  var lettersPressed = "";
+   function reset() { 
+   userGuesses = 9; 
+   userLosses = 0;
+   userWins = 0;
+   lettersPressed = 0;
+  };
+  // var chosenLetters = compLetters[Math.floor(Math.random() * compLetters.length)];
+  //user presses key
+
+  document.onkeyup = function(event) {
+    lettersPressed = lettersPressed + event.key;
+
+    var chosenLetters = compLetters[Math.floor(Math.random() * compLetters.length)];
+    console.log(chosenLetters);
+    document.getElementById("your-guesses").textContent = "Your Guesses So Far :" + lettersPressed;
+    //make a condition if userInput will 9 times
+    if (event.key === chosenLetters) {
+      document.getElementById("wins").textContent = "Wins :" + userWins;
+      userWins++;
+      alert("you win");
+      reset();
+    } else {
+      // userGuesses = 9;
+      alert("your lost");
+      document.getElementById("guesses-left").textContent = "Guesses Left :" + userGuesses;
+      userGuesses--;
+      // else { (userLosses = 9 && userGuesses === 0) 
+      document.getElementById("losses").textContent = "Losses :" + userLosses;
+      userLosses++;
+      console.log(userGuesses);
+    } if (userLosses == 9 && userGuesses == 0) 
+      reset();
+  }
